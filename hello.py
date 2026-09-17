41847ce90fa2c0c6c702dddef47d2bf594a0a3a0
gautamsanjok32 <gautamsanjok32@gmail.com>
2026-09-17 17:27:40 +0545

    learning interactions through genai

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("hello, world")
+from google import genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+client=genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY"),
+)
+interaction= client.interactions.create(
+    model= "gemini-3.8-flash",
+    input="what is andromeda galaxy? ",
+    generation_config={
+        "thinking_level":"low"
+    }
+
+)
+print(interaction.output_text)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from sahayak!")
-
-
-if __name__ == "__main__":
-    main()
