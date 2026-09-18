3c6fd2abd83e9a7ae9ae2cfda96e01560ec25d57
gautamsanjok32 <gautamsanjok32@gmail.com>
2026-09-18 15:42:50 +0545

    system pormpt

--- hello.py
+++ hello.py
@@ -2,19 +2,12 @@ from google import genai
 from dotenv import load_dotenv
 import os
 load_dotenv()
-
 client=genai.Client(
     api_key=os.getenv("GEMINI_API_KEY"),
 )
-stream= client.interactions.create(
-    model= "gemini-3.8-flash",
-    input="what is andromeda galaxy? ",
-    generation_config={
-        "thinking_level":"low"
-    },
-    stream=True,
+interaction=client.interactions.create(
+    model="gemini-3.8-flash",
+    system_instruction="Your name is Sanjog Gautam. You are a BSC.CSIT student at 6th semester",
+    input="Hello there!"
 )
-for event in stream:
-    if event.event_type == "step.delta":
-        if event.delta.type=="text":
-            print(event.delta.text,end="",flush=True)
+print(interaction.output_text)
